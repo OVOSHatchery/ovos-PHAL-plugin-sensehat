a511993322aa230c49ff31fcf21083aff5f054f6
jarbasal <jarbasai@mailfence.com>
2018-10-21 22:34:11 +0100

    easy install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SenseHat_enclosure',
+    version='0.1',
+    packages=['sensehat_enclosure'],
+    url='',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    description='sensehat for mycroft'
+)
